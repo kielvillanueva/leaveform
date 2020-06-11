c2fb7c7e45caaef9ba5490e2fb6422c7bbb524fc
kielvillanueva <jvillanueva@edukasyon.ph>
2020-06-11 11:45:27 +0800

    Initial working leave form commit

--- db.sql
+++ db.sql
@@ -1,14 +1,26 @@
-CREATE DATABASE inventory;
+CREATE DATABASE company;
 
-use inventory;
+use company;
 
-CREATE TABLE items (
+CREATE TABLE users (
     id int NOT NULL,
     name varchar(200) NOT NULL,
-    qty int NOT NULL,
-    amount float NOT NULL
+    leaves float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-ALTER TABLE items ADD PRIMARY KEY (id);
+ALTER TABLE users ADD PRIMARY KEY (id);
 
-ALTER TABLE items MODIFY id int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE users MODIFY id int(11) NOT NULL AUTO_INCREMENT;
+
+CREATE TABLE leaves (
+    id int NOT NULL,
+    user_id int NOT NULL,
+    beginning DATE NOT NULL,
+    ending DATE NULL,
+    type varchar(200) NOT NULL,
+    length float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+ALTER TABLE leaves ADD PRIMARY KEY (id);
+
+ALTER TABLE leaves MODIFY id int(11) NOT NULL AUTO_INCREMENT;
--- server.js
+++ server.js
@@ -2,141 +2,191 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
+const cookieParser = require('cookie-parser');
   
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+app.use(cookieParser())
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
   
 var db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'inventory'
+    database: 'company'
 });
 
 db.connect();
 console.log('Successfully connected to DB');  
- 
-/**All Items Endpoint */
-app.get('/items', (req, res) => {
 
-    db.query('SELECT * FROM items', [], (error, results) => {
+app.get('/', (req, res) => {
 
-        if(error) throw error
-
-        console.log('Rows Returned: ', results.length);
-        return res.send({
-            code: 200, 
-            data: results, 
-            message: 'Items list.'
-        });
-
-    });
+    res.cookie('user', null);
+    res.render('index', { title: 'Welcome Page'});
 
 });
- 
- 
-/**Item Endpoint */
-app.get('/item/:id', (req, res) => {
-  
-    let itemId = req.params.id;
-  
-    if (!itemId) {
-        return res.status(400).send({ code: 400, message: 'Please provide ID' });
-    }
-  
-    db.query('SELECT * FROM items where id=?', itemId, function (error, results) {
 
-        if (error) throw error;
+app.get('/user', (req, res) => {
+
+    if(!req.cookies['user']) {
+        res.redirect('/');
+    }
+    res.render('userHome', { title: 'User Home Page', name: req.cookies['user'], announcement: false });
+
+});
+
+app.post('/user', (req, res) => {
+
+    if(!req.body['user']) {
+        res.redirect('/');
+    }
+    var name = req.body['user'];
+
+    db.query('SELECT * FROM users where name=?', name, function (error, results) {
+
+        if (error) return res.render('500', { title: 'Error Page', error});
         
         console.log('Rows Returned: ', results.length);
-        return res.send({ 
-            code: 200,
-            data: results[0], 
-            message: 'Single item.' 
-        });
+
+        if(results.length == 0) {
+            var user = {
+                name
+            };
+
+            const numberOfLeaves = 15; // Assumed 15 days of leaves per year
+            const monthToday = new Date().getMonth() +1;
+            user['leaves'] = Math.floor((monthToday / 12) * numberOfLeaves); // Accumulated Leaves for this year
+
+
+            db.query("INSERT INTO users SET ? ", user, function (error, results, fields) {
+
+                if (error) return res.render('500', { title: 'Error Page', error});
+        
+                res.cookie('user', name);
+                return res.render('userHome', { title: 'User Home Page', name, announcement: false });
+        
+            });
+
+        } else {
+            res.cookie('user', name);
+            return res.render('userHome', { title: 'User Home Page', announcement: false, ...results[0]});
+        }
 
     });
 
 });
- 
 
-/**Add Item Endpoint */
-app.post('/item', (req, res) => {
-  
-    let item = req.body;
-  
-    if (!item.name || !item.qty || !item.amount) {
-        return res.status(400).send({ code: 400, message: 'Please provide name, qty, amount' });
+app.get('/user/leaves', (req, res) => {
+
+    if(!req.cookies['user']) {
+        res.redirect('/');
     }
-  
-    db.query("INSERT INTO items SET ? ", item, function (error, results, fields) {
+    var name = req.cookies['user'];
 
-        if (error) throw error;
+    db.query('SELECT * FROM users where name=?', name, function (error, results) {
 
-        return res.send({ 
-            code: 200, 
-            data: results, 
-            message: 'Item has been created successfully.' 
-        });
+        if (error) return res.render('500', { title: 'Error Page', error});
+        
+        if(results.length == 0) {
+            res.redirect('/');
+        } else {
+
+            var remainingLeaves = results[0]['leaves'];
+            var user_id = results[0]['id'];
+            db.query('SELECT * FROM leaves where user_id=?', user_id, function (error, results) {
+
+                if (error) return res.render('500', { title: 'Error Page', error});
+
+                return res.render('leaves', { title: 'Leaves Page', name: req.cookies['user'], leaves: remainingLeaves, results: results});
+        
+            });
+        }
 
     });
 
 });
- 
 
-/**Update Item Endpoint */
-app.put('/item/:id', (req, res) => {
-  
-    let itemId = req.params.id;
-    let item = req.body;
-  
-    if (!itemId) {
-        return res.status(400).send({ error: item, message: 'Please provide itemId' });
+app.get('/user/file', (req, res) => {
+
+    if(!req.cookies['user']) {
+        res.redirect('/');
     }
-    
-    if (!item) {
-        return res.status(400).send({ error: item, message: 'Please provide either name, qty or amount' });
+
+    res.render('file', { title: 'Filing of Leaves Page', name: req.cookies['user'], error: false });
+
+});
+
+app.post('/user/file', (req, res) => {
+
+    if(!req.cookies['user']) {
+        res.redirect('/');
     }
-  
-    db.query("UPDATE items SET ? WHERE id = ?", [item, itemId], function (error, results, fields) {
+    var name = req.cookies['user'];
 
-        if (error) throw error;
+    const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const beginning = new Date(req.body['beginning']);
+    const ending = new Date(req.body['ending']);
+    var days = [];
 
-        return res.send({ 
-            code: 200,
-            data: results, 
-            message: 'Item has been updated successfully.' 
-        });
+    for(var d = beginning; d <= ending; d.setDate(d.getDate() + 1)) {
+        days.push(weekdays[d.getDay()]);
+    }
+
+    const weekends = ['Saturday', 'Sunday'];
+    for(var key in weekends) {
+        var index = days.indexOf(weekends[key]);
+        while (index > -1) {
+            days.splice(index, 1);
+            index = days.indexOf(weekends[key]);
+        }
+    }
+
+    db.query('SELECT * FROM users where name=?', name, function (error, results) {
+
+        if (error) return res.render('500', { title: 'Error Page', error});
+        
+        var user_id = results[0]['id'];
+        if(results.length == 0) {
+            res.redirect('/');
+        } else if(results[0]['leaves'] < days.length) {
+            return res.render('file', { title: 'Filing of Leaves Page', name, error: 'Not Enough Leaves' });
+        } else {
+
+            var length = req.body['type'] == 'Full Day' ? days.length : 0.5;
+            var user = {
+                leaves: results[0]['leaves'] - length
+            }
+            db.query("UPDATE users SET ? WHERE id = ?", [user, user_id], function (error, results, fields) {
+
+                if (error) return res.render('500', { title: 'Error Page', error});
+
+                var leave = {
+                    user_id: user_id,
+                    length,
+                    beginning,
+                    ending,
+                    type: req.body['type']
+                }
+                db.query("INSERT INTO leaves SET ? ", leave, function (error, results, fields) {
+
+                    if (error) return res.render('500', { title: 'Error Page', error});
+
+                    res.cookie('user', name);
+                    return res.render('userHome', { title: 'User Home Page', name, announcement: 'Successfully filed!', error: false });
+            
+                });
+                
+            });
+
+        }
 
     });
 
 });
- 
-
-/**Delete Item Endpoint */
-app.delete('/item/:id', (req, res) => {
-  
-    let itemId = req.params.id;
-  
-    if (!itemId) {
-        return res.status(400).send({ error: true, message: 'Please provide itemId' });
-    }
-
-    db.query('DELETE FROM items WHERE id = ?', [itemId], function (error, results, fields) {
-
-        if (error) throw error;
-
-        return res.send({ 
-            code: 200,
-            data: results, 
-            message: 'Item has been deleted successfully.' 
-        });
-
-    });
-
-}); 
  
 app.listen(3000, () => {
     console.log('Node app is running on port 3000');
